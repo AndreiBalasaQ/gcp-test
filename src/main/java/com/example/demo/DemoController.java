a59fc5f74b0a5f4e4cb5e9f7dea82269fd377b4d
andreitomaQ <andrei.toma@qualitance.com>
2023-11-12 16:06:13 +0200

    another one

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -9,6 +9,6 @@ public class DemoController {
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("hello, boys4");
+        return ResponseEntity.ok("hello, boys5");
     }
 }

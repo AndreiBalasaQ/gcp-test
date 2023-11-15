01ebc8545f9fd96d8f5fbf809c4442232f5715ab
andreitomaQ <andrei.toma@qualitance.com>
2023-11-15 17:17:55 +0200

    ne wpush

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -9,6 +9,6 @@ public class DemoController {
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("hello, boys6");
+        return ResponseEntity.ok("hello, girls");
     }
 }
